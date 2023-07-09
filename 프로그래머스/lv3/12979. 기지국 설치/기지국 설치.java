d4f141e2fb1d59f160e49f99679add2adbd7b25e
tjsdn9803 <80503834+tjsdn9803@users.noreply.github.com>
2023-07-09 20:34:02 +0900

    [level 3] Title: 기지국 설치, Time: 0.60 ms, Memory: 52.3 MB -BaekjoonHub

--- 프로그래머스/lv3/12979. 기지국 설치/기지국 설치.java
+++ 프로그래머스/lv3/12979. 기지국 설치/기지국 설치.java
@@ -3,11 +3,6 @@ class Solution {
         int startPoint = 1;
         int endPoint = 1;
         int answer = 0;
-        // station배열의 기지국의 전파가 도달하는 가장 왼쪽칸(startPoint)과 전파가 닿지 않는 구역의 가장 왼쪽 칸(endPoint)의            차이를 구하여 기지국 배정
-        // 그다음 station의 도달하는 전파가 endPoint위치보다 왼쪽에 있을 수 있으므로 차이를 구하여 1이상일때만 로직 발동
-        
-        // ex) 예시 1의 경우 startPoint는 3 endPoint는 1, 차이가 2이기 때문에 기지국 한개 추가 
-        //     다음 기지국의 startPoint는 10 endPoint는 6, 차이가 4이기 떄문에 기지국 두개 추가
         for(int i = 0; i < stations.length; i++){
             startPoint = stations[i] - w;
             if(startPoint - endPoint >= 1){
@@ -15,8 +10,6 @@ class Solution {
             }
             endPoint = startPoint + (2*w+1);
         }
-        
-        //     반복문이 종료되고 난 뒤 남아있는 전파가 닿지 않는 곳이 있는지 확인 후 있다면 기지국 추가
         if(endPoint <= n){
             answer += (n+1 - endPoint - 1) / (2*w+1) + 1;
         }
